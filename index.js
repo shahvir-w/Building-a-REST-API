8cc34ce8135f4607a080743424c4b4b2300e1faf
shahvir-w <shahvir05@icloud.com>
2025-01-05 19:16:22 -0500

    PUT and DELETE methods

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ const app = express();
 
 app.use(express.json());
 
+mongoose.connect("mongodb+srv://shahvir:uHkRqXOM9QiDcOv6@cluster0.7uz6z.mongodb.net/Node-API?retryWrites=true&w=majority&appName=Cluster0")
+.then(() => {
+    console.log("connected to database");
+    app.listen(3000, () => console.log('server is running'))
+})
+.catch(() => {
+    console.log("connection failed")
+})
 
 app.get('/', (req, res) => {
     res.send('Hello from Node API')
@@ -22,15 +30,8 @@ app.get('/api/products', async (req, res) => {
 });
 
 // get a specific product
-app.get('/api/products/:id', async (req, res) => {
-    try {
-        const { id } = req.params;
-        const product = await Product.findById(id);
-        res.status(200).json(product);
-
-    } catch (error) {
-        res.status(500).json({message: error.message});
-    }
+app.get('/api/products/:id', getProduct, (req, res) => {
+    res.json(res.product)
 });
 
 // add a product
@@ -48,11 +49,43 @@ app.post('/api/products', async (req, res) => {
     }
 });
 
-mongoose.connect("mongodb+srv://shahvir:uHkRqXOM9QiDcOv6@cluster0.7uz6z.mongodb.net/Node-API?retryWrites=true&w=majority&appName=Cluster0")
-.then(() => {
-    console.log("connected to database");
-    app.listen(3000, () => console.log('server is running'))
+// update a product
+app.put('/api/products/:id', async (req, res) => {
+    try {
+        const {id} = req.params;
+        const product = await Product.findByIdAndUpdate(id, req.body)
+        if (!product) {
+            return res.status(404).json({ message: "Product not found" });
+        }
+        const updatedProduct = await Product.findById(id);
+        res.status(200).json(updatedProduct);
+        
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+//delete a product
+app.delete('/api/products/:id', getProduct, async (req,res) => {
+    try {
+        await res.product.deleteOne();
+        res.status(200).json({message: "deleted product"})
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
 })
-.catch(() => {
-    console.log("connection failed")
-})
+
+// middleware to get product by ID
+async function getProduct(req, res, next) {
+    let product;
+    try {
+        product = await Product.findById(req.params.id)
+        if (product == null) {
+            return res.status(404).json({message: 'Cannot find product'})
+        }
+    } catch (error) {
+        return res.status(500).json({message: error.message})
+    }
+    res.product = product;
+    next();
+}
